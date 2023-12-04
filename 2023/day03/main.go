a40803130b326df5e6be9962fecee2adb14f39f4
Kyler Wilson <kyler.wilson.1@pm.me>
2023-12-03 19:46:48 -0800

    day03 completed

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -12,14 +12,18 @@ import (
 //go:embed input.txt
 var input string
 
+type Symbol struct {
+	symbol string
+	line   int
+	idx    int
+	parts  []Part
+}
+
 type Part struct {
 	number     int
 	startIndex int
 	endIndex   int
-	truePart   bool
-	nextTo     string
-	symbolLine int
-	smybolIdx  int
+	legal      bool
 }
 
 func init() {
@@ -33,12 +37,12 @@ func main() {
 	var part, ans int
 	flag.IntVar(&part, "part", 1, "part 1 or part 2")
 	flag.Parse()
-	lines := parse_input(input)
+	parts, symbols := parse_input(input)
 
 	if part == 1 {
-		ans = solve_part_1(lines)
+		ans = solve_part_1(parts)
 	} else {
-		ans = solve_part_2(lines)
+		ans = solve_part_2(symbols)
 	}
 
 	fmt.Printf("Part %d: %d\n", part, ans)
@@ -46,98 +50,140 @@ func main() {
 
 func solve_part_1(parts []Part) (ans int) {
 	for _, part := range parts {
-		if part.truePart {
+		if part.legal {
 			ans += part.number
 		}
 	}
 	return
 }
 
-func solve_part_2(parts []Part) (ans int) {
+func solve_part_2(symbols []Symbol) (ans int) {
+	for _, s := range symbols {
+		if s.symbol == "*" && len(s.parts) == 2 {
+			ans += (s.parts[0].number * s.parts[1].number)
+			continue
+		}
+	}
 	return
 }
 
-func parse_input(input string) (parts []Part) {
-	input = strings.ReplaceAll(strings.TrimRight(input, "\n"), ".", " ")
+func parse_input(input string) (parts []Part, symbols []Symbol) {
+	input = strings.TrimRight(input, "\n")
 
 	lines := strings.Split(input, "\n")
-	length := len(lines)
-	partNumberIdxs := make([][][]int, length)
-	partIndecatorIdxs := make([][][]int, length)
+	ll := len(lines)
+
+	partNumberIdxs := make([][][]int, ll)
+	partIndecatorIdxs := make([][][]int, ll)
+
+	rpn, err := regexp.Compile("([0-9]+)")
+	if err != nil {
+		panic(err)
+	}
+
+	rpi, err := regexp.Compile("([*+#$/=@%&-])")
+	if err != nil {
+		panic(err)
+	}
 
 	for i, line := range lines {
-		regexPartNumber, err := regexp.Compile("([0-9]+)")
-		if err != nil {
-			panic(err)
-		}
-		partNumberMatches := regexPartNumber.FindAllStringSubmatchIndex(line, -1)
-
-		regexPartIndecators, err := regexp.Compile("([*+#$/=@%&-])")
-		if err != nil {
-			panic(err)
-		}
-		partIndecatorMatches := regexPartIndecators.FindAllStringSubmatchIndex(line, -1)
+		partNumberMatches := rpn.FindAllStringSubmatchIndex(line, -1)
+		partIndecatorMatches := rpi.FindAllStringSubmatchIndex(line, -1)
 
 		partNumberIdxs[i] = partNumberMatches
 		partIndecatorIdxs[i] = partIndecatorMatches
 	}
 
-	for i := 0; i < length; i++ {
+	for i := 0; i < ll; i++ {
+		for j := 0; j < len(partIndecatorIdxs[i]); j++ {
+			if len(partIndecatorIdxs[i][j]) <= 0 {
+				continue
+			}
+			// go to that symbol index
+			s := &Symbol{
+				symbol: lines[i][partIndecatorIdxs[i][j][0]:partIndecatorIdxs[i][j][1]],
+				line:   i,
+				idx:    partIndecatorIdxs[i][j][0],
+			}
 
+			s.findNumsAroundSymbols(partNumberIdxs[i-1], lines[i-1])
+			s.findNumsAroundSymbols(partNumberIdxs[i], lines[i])
+			s.findNumsAroundSymbols(partNumberIdxs[i+1], lines[i+1])
+
+			// we want to check to see if there's numbers touching it
+			symbols = append(symbols, *s)
+			// if there is, then we want to add it to the part slice in the symbol object
+		}
 		for j := 0; j < len(partNumberIdxs[i]); j++ {
 			if len(partNumberIdxs[i][j]) <= 0 {
 				continue
 			}
-			// check if there's an indecator near it
-			part := Part{
+
+			p := Part{
 				number:     0,
-				startIndex: 0,
-				endIndex:   0,
-				truePart:   false,
+				startIndex: partNumberIdxs[i][j][0],
+				endIndex:   partNumberIdxs[i][j][1],
+				legal:      false,
 			}
 
-			part.startIndex = partNumberIdxs[i][j][0]
-			part.endIndex = partNumberIdxs[i][j][1]
-			n, err := strconv.Atoi(lines[i][part.startIndex:part.endIndex])
+			if i != 0 {
+				findIndecatorIdx(partIndecatorIdxs[i-1], lines[i-1], i-1, &p)
+			}
+			findIndecatorIdx(partIndecatorIdxs[i], lines[i], i, &p)
+			if i < ll-1 {
+				findIndecatorIdx(partIndecatorIdxs[i+1], lines[i+1], i+1, &p)
+			}
+
+			n, err := strconv.Atoi(lines[i][p.startIndex:p.endIndex])
 			if err != nil {
 				panic(err)
 			}
 
-			if i != 0 {
-				for _, inds := range partIndecatorIdxs[i-1] {
-					if inds[0] >= part.startIndex-1 && inds[1] <= part.endIndex+1 {
-						part.truePart = true
-						part.nextTo = lines[i-1][inds[0]:inds[1]]
-						part.symbolLine = i - 1
-						part.smybolIdx = inds[0]
-					}
-				}
+			p.number = n
+
+			if p.legal {
+				parts = append(parts, p)
 			}
-			for _, inds := range partIndecatorIdxs[i] {
-				if inds[0] >= part.startIndex-1 && inds[1] <= part.endIndex+1 {
-					part.truePart = true
-					part.nextTo = lines[i][inds[0]:inds[1]]
-					part.symbolLine = i
-					part.smybolIdx = inds[0]
-				}
-			}
-			if i < length-1 {
-				for _, inds := range partIndecatorIdxs[i+1] {
-					if inds[0] >= part.startIndex-1 && inds[1] <= part.endIndex+1 {
-						part.truePart = true
-						part.nextTo = lines[i+1][inds[0]:inds[1]]
-						part.symbolLine = i + 1
-						part.smybolIdx = inds[0]
-					}
-				}
+		}
+	}
+
+	return
+}
+
+func (s *Symbol) findNumsAroundSymbols(nums [][]int, line string) {
+	//build part
+	var parts []Part
+	for _, v := range nums {
+		if (v[0] >= s.idx-1 && v[0] <= s.idx+1) || (v[1]-1 >= s.idx-1 && v[1]-1 <= s.idx+1) {
+			part := Part{
+				startIndex: v[0],
+				endIndex:   v[1],
+				number:     0,
+				legal:      true,
 			}
 
-			part.number = n
+			num, err := strconv.Atoi(line[part.startIndex:part.endIndex])
+
+			if err != nil {
+				panic(err)
+			}
+
+			part.number = num
+
 			parts = append(parts, part)
 		}
 	}
 
-	fmt.Println(parts)
+	s.parts = append(s.parts, parts...)
+}
 
-	return
+func findIndecatorIdx(inds [][]int, line string, idx int, part *Part) {
+	if part.legal {
+		return
+	}
+	for _, ind := range inds {
+		if ind[0] >= part.startIndex-1 && ind[1] <= part.endIndex+1 {
+			part.legal = true
+		}
+	}
 }
